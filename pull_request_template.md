fbe99bf5ac3c59d8e072d605a4d17bc3ea55dc0d
Sascha Mirow-Stanitzok <Sascha.Mirow-Stanitzok@md.de>
2021-01-25 16:50:09 +0100

    feat: strict

--- pull_request_template.md
+++ pull_request_template.md
@@ -1,7 +1,7 @@
 ### All Submissions:
 
-* [ ] Have you followed the guidelines in our Contributing document?
-* [ ] Have you checked to ensure there aren't other open [Pull Requests](../../../pulls) for the same update/change?
+- [ ] Have you followed the guidelines in our Contributing document?
+- [ ] Have you checked to ensure there aren't other open [Pull Requests](../../../pulls) for the same update/change?
 
 ### New Feature Submissions:
 
@@ -10,6 +10,6 @@
 
 ### Changes to Core Features:
 
-* [ ] Have you added an explanation of what your changes do and why you'd like us to include them?
-* [ ] Have you written new tests for your core changes, as applicable?
-* [ ] Have you successfully ran tests with your changes locally?
+- [ ] Have you added an explanation of what your changes do and why you'd like us to include them?
+- [ ] Have you written new tests for your core changes, as applicable?
+- [ ] Have you successfully ran tests with your changes locally?
--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,47 @@ const slsw = require('serverless-webpack')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  mode: 'production',
-  externals: [{'aws-sdk': 'commonjs aws-sdk'}],
-  resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true
+    entry: slsw.lib.entries,
+    target: 'node',
+    mode: 'production',
+    externals: [{'aws-sdk': 'commonjs aws-sdk'}],
+    resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true
+                        }
+                    }
+                ]
             }
-          }
         ]
-      }
+    },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js'
+    },
+    node: {
+        __dirname: false
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            /[\/\\]node_modules[\/\\]timezonecomplete[\/\\]/,
+            path.resolve('node_modules/tzdata/'),
+            {
+                'tzdata-backward-utc': 'tzdata-backward-utc',
+                'tzdata-etcetera': 'tzdata-etcetera',
+                'tzdata-europe': 'tzdata-europe'
+            }
+        )
     ]
-  },
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js'
-  },
-  node: {
-    __dirname: false
-  },
-  plugins: [
-    new webpack.ContextReplacementPlugin(
-      /[\/\\]node_modules[\/\\]timezonecomplete[\/\\]/,
-      path.resolve('node_modules/tzdata/'),
-      {
-        'tzdata-backward-utc': 'tzdata-backward-utc',
-        'tzdata-etcetera': 'tzdata-etcetera',
-        'tzdata-europe': 'tzdata-europe'
-      }
-    )
-  ]
 }
